9b9903da31720564af002454e079771e8e731912
giero202 <80042934+giero202@users.noreply.github.com>
2021-11-06 15:15:42 +0700

    fix bug mat khau

--- dao/sign-up.php
+++ dao/sign-up.php
@@ -10,7 +10,7 @@ function checkEmail($email){
 
 function addUser($random_id,$fname,$lname,$email,$pass,$final_img,$user_status,$gender,$phone,$user_name){
     $sql = " INSERT INTO users (unique_id, fname, lname, email, pass, img, user_status,gender,  phone, user_name) 
-            VALUE ({$random_id}, '{$fname}','{$lname}','{$email}',{$pass},'{$final_img}','{$user_status}' , '{$gender}' ,'{$phone}','{$user_name}' )";
+            VALUE ({$random_id}, '{$fname}','{$lname}','{$email}','{$pass}','{$final_img}','{$user_status}' , '{$gender}' ,'{$phone}','{$user_name}' )";
      return pdo_execute($sql);
     
 }
